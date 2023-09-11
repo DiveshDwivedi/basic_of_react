cd8b82797782f0b484ce6e10fc020e384710ffd4
Divesh Dwivedi <dwivedidivesh21@gmail.com>
2023-09-11 23:50:54 +0530

    feat: todo list basic

--- src/App.js
+++ src/App.js
@@ -1,34 +1,35 @@
 import "./App.css";
 import { useState } from "react";
+import { Task } from "./Task";
 
 function App() {
-// Day - 7
-  // ToDo List 
+  // Day - 7
+  // ToDo List
   const [todoList, setTodoList] = useState([]);
   const [newTask, showNewTask] = useState("");
 
-const handleChange = (event) => {
-  showNewTask(event.target.value);
-}
-
-const addTask = () => {
-  // const newToDoList = [...todoList, newTask];
-  // setTodoList(newToDoList);
-
-  // optimize
-  // setTodoList([...todoList, newTask]);  
-
-  // so delete on the basis of id instead of name
-  
-  const task = {
-    id: todoList.length === 0 ? 1 : todoList[todoList.length-1].id+1,
-    name: newTask,
+  const handleChange = (event) => {
+    showNewTask(event.target.value);
   };
-  setTodoList([...todoList, task]);  
 
-}
+  const addTask = () => {
+    // const newToDoList = [...todoList, newTask];
+    // setTodoList(newToDoList);
 
-/*
+    // optimize
+    // setTodoList([...todoList, newTask]);
+
+    // so delete on the basis of id instead of name
+
+    const task = {
+      id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id + 1,
+      name: newTask,
+      completed: false,
+    };
+    setTodoList([...todoList, task]);
+  };
+
+  /*
 const deleteTask = (taskName) => {
     const newToDoListAfterDelete = todoList.filter((task) => {
       // if (taskName === task) {
@@ -41,20 +42,31 @@ const deleteTask = (taskName) => {
       return taskName !== task;
     });
 */
-    // more optimize 
-    
-//       const deleteTask = (taskName) => {
-//       // const newToDoListAfterDelete = todoList.filter((task) => taskName !== task);
-//       // setTodoList([newToDoListAfterDelete]);
+  // more optimize
 
-//       setTodoList(todoList.filter((task) => taskName !== task));
-// }
+  //       const deleteTask = (taskName) => {
+  //       // const newToDoListAfterDelete = todoList.filter((task) => taskName !== task);
+  //       // setTodoList([newToDoListAfterDelete]);
 
-// so delete using key 
-const deleteTask = (id) => {
-  setTodoList(todoList.filter((task) => task.id !== id));
-}
+  //       setTodoList(todoList.filter((task) => taskName !== task));
+  // }
 
+  // so delete using key
+  const deleteTask = (id) => {
+    setTodoList(todoList.filter((task) => task.id !== id));
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          return { ...task, completed: true };
+        } else {
+          return task;
+        }
+      })
+    );
+  };
   return (
     <div className="App">
       <div className="addTask">
@@ -62,16 +74,14 @@ const deleteTask = (id) => {
         <button onClick={addTask}>Add Task</button>
       </div>
       <div className="lsit">
-        {
-          todoList.map((task) => {
-            return <div className="task">
-              <h1>{task.name}</h1>
-              <button onClick={() => deleteTask(task.id)}>X</button>
-            </div> 
-          })
-        }
+        {todoList.map((task) => {
+          // change below logic to component
+          return (
+            <Task taskName={task.name} id={task.id} completed={task.completed} deleteTask={deleteTask}  completeTask={completeTask} />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
- export default App;
+export default App;
